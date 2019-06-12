7fcb1ed26aa10c85730b3299dfc61baf176d43ae
Erica Hawkins <ehawk901@gmail.com>
2019-06-11 22:26:56 -0500

    Grandma Commit

--- grandma.rb
+++ grandma.rb
@@ -1,17 +1,12 @@
 def speak_to_grandma(phrase)
 
-if speak_to_grandma == "I LOVE YOU GRANDMA!".upcase 
-    puts "I LOVE YOU TOO PUMPKIN!"
+if phrase == "I LOVE YOU GRANDMA!"
+     "I LOVE YOU TOO PUMPKIN!"
 
-elsif speak_to_grandma == "Hi Nana, how are you?".downcase 
-    puts "HUH?! SPEAK UP, SONNY!"
-
-elsif speak_to_grandma == "Hi!".downcase
-    puts "HUH?! SPEAK UP, SONNY!"
-
-elsif speak_to_grandma == "WHAT DID YOU EAT TODAY?".upcase
-    puts "NO, NOT SINCE 1938!"
+elsif phrase == phrase.upcase
+     "NO, NOT SINCE 1938!"
 
 else
-    puts "NO, NOT SINCE 1938!"    
-end 
+    "HUH?! SPEAK UP, SONNY!"  
+end    
+end
